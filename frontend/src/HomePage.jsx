77272200703277bfa5dc46a977b9d5e211a62830
Soham Ghosh <ghoshsoham1708@gmail.com>
2025-04-04 19:58:34 +0530

    Home Page created

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router";
+import '../styles/HomePage.scss'
 export const HomePage = () => {
   return (
-    <div>
-      <p>HomPage</p>
+    <div className="home-page">
+      <div className="leftpane">
+        <h1>Welcome To Neutron!</h1>
+        <h2>Capture ideas effortlessly. Our AI-powered app organizes, summarizes, and syncs your notes across devices. Stay productive with contextual search and automatic reminders. Start organizing smarter today!</h2>
+        {/* <h2>Notes managed by AI, ... and more!</h2> */}
+        <Link className="link_button" to={"/dashboard"}>Get Started</Link>
+      </div>
+      <div className="rightpane"><img className="right_image" src="\images\one.png"></img></div>
     </div>
   );
 };
